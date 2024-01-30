8c4f6d0d6e6ae84a4ba2f0f4610f3cb25d6d9534
Poland 7MC <poland7mc@Polyak-3.local>
2024-01-30 11:12:56 +0300

    ZZZ

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,8 +1,6 @@
 pipeline {
     agent {label ('slave1')}
-    tools {
-  'org.jenkinsci.plugins.docker.commons.tools.DockerTool' 'docker'
-    }
+    tools { 'org.jenkinsci.plugins.docker.commons.tools.DockerTool' 'docker'}
 
     stages {
         stage("Checkout") {
