4afc6f474413c2db303ad5d0827b9216e4cb1216
Poland 7MC <poland7mc@Polyak-3.local>
2024-02-19 16:50:01 +0300

    pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -23,7 +23,7 @@ pipeline {
             steps {
                 script {
                     docker.withRegistry('https://hub.docker.com/r/polyak313/nginx', 'dockerhub-cred-polyak313') {
-                        def customImage = docker.build('nginx/Dockerfile', '')
+                        def customImage = docker.build('polyak313/nginx', 'nginx')
                         customImage.push('latest')
                     }
                 }
